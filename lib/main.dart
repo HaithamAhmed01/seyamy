1a2ffdc80952f15a6d20453990d7498e5583f8a7
HaithamAhmed01 <54605034+HaithamAhmed01@users.noreply.github.com>
2023-07-09 12:15:58 +0300

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'core/utils/bloc_observer.dart';
 import 'core/utils/injection_container.dart' as di;
 import 'my_app.dart';
 
+// https://www.figma.com/file/xbC7UNs6gFplITl2KfJqE5/SEYAMY-FINAL?type=design&node-id=184%3A1761&mode=design&t=hCFnbjBKk8laUXUy-1
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
